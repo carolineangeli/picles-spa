309b8b09752d9faceafd37c895687b233741ec72
Caroline Angeli <caroline_angeli@hotmail.com>
2024-04-15 22:18:29 -0300

    botoões ganhando forma

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import { Button } from './components/common/Button'
+import { useState } from 'react';
+import { Button } from './components/common/Button';
+import { ButtonVariant } from './components/common/Button/Button.constants';
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       {count}
-      <Button onClick={() => setCount(count + 1)}>Quero adotar</Button>
+      <Button variant={ButtonVariant.Default} onClick={() => setCount(count + 1)}>Quero Adotar</Button>
+      <Button variant={ButtonVariant.Outlined} onClick={() => setCount(count + 1)}>Tenho um abrigo</Button>
     </>
-    
-  )
+  );
 }
