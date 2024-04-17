4dc2c5190f111baedb6897a50ca963d8a81a3e41
Caroline Angeli <caroline_angeli@hotmail.com>
2024-04-16 21:12:55 -0300

    commit salvador

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,3 +1,22 @@
-export function Header(){
-    return<></>
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+import { Button, ButtonVariant } from '../Button';
+
+interface IHeader {
+  showReturn?: boolean;
+}
+
+export function Header({ showReturn }: IHeader) {
+  return (
+    <header className={styles.header}>
+      {showReturn && (
+        <Link to="/pets">
+          <Button variant={ButtonVariant.Text}>Voltar</Button>
+        </Link>
+      )}
+      <Link to="/admin">
+        <Button variant={ButtonVariant.Outlined}>Tenho um abrigo</Button>
+      </Link>
+    </header>
+  );
 }
