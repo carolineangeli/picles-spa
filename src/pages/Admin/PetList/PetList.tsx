c13ee269a143314f7d6770eab7efdbd0855356b9
Caroline Angeli <caroline_angeli@hotmail.com>
2024-04-23 20:04:39 -0300

    criando pagina admin

---
+++ src/pages/Admin/PetList/PetList.tsx
@@ -0,0 +1,4 @@
+import { Panel } from "../../../components/layout/Panel"
+export function PetList(){
+    return <Panel>Listagens de pets do abrigo</Panel>
+}
---
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -0,0 +1,5 @@
+import { Panel } from "../../../components/layout/Panel"
+
+export function Shelter(){
+    return <Panel>Formulario de abrigo</Panel>
+}
