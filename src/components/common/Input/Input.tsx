f1068e2669375288d8073a26d60d61ca04f41332
Caroline Angeli <caroline_angeli@hotmail.com>
2024-04-24 20:50:53 -0300

    adicionando

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,19 +1,17 @@
-import { InputHTMLAttributes, forwardRef } from 'react'
+import { InputHTMLAttributes, Ref, forwardRef } from 'react'
 import styles from './Input.module.css'
 
-interface IInput extends
-    InputHTMLAttributes<HTMLInputElement> {
-    label: string
+interface IInput extends InputHTMLAttributes<HTMLInputElement> {
+  label: string
 }
 
 function ForwardInput({ label, ...rest }: IInput, ref: Ref<HTMLInputElement>) {
-    return (
-        <div className={styles.inputGroup}>
-            <label>{label}</label>
-            <input ref={ref} {...rest} />
-        </div>
-    )
+  return (
+    <div className={styles.inputGroup}>
+      <label>{label}</label>
+      <input ref={ref} {...rest} />
+    </div>
+  )
 }
 
 export const Input = forwardRef(ForwardInput)
-
