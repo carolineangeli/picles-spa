0a283e62de94831597fbb0a47f4ca15bf7b12eb9
Caroline Angeli <caroline_angeli@hotmail.com>
2024-04-23 21:28:18 -0300

    ajuste final

--- src/components/common/Sidebar/Sidebar.tsx
+++ src/components/common/Sidebar/Sidebar.tsx
@@ -1,10 +1,30 @@
 import { NavLink } from 'react-router-dom'
 import styles from './Sidebar.module.css'
+import { Toaster, toast } from 'sonner'
 
 export function Sidebar() {
-  return <nav className={styles.sidebar}>
-    <NavLink className={({isActive}) => (isActive? styles.active : '') } to="/admin" end>Meu Abrigo</NavLink>
-    <NavLink className={({isActive}) => (isActive? styles.active : '') } to="/admin/pets">Pets</NavLink>
-    <NavLink to="/">Sair</NavLink>
-  </nav>
+    function validate(event: React.MouseEvent) {
+        const canAccess = false
+        if (!canAccess) {
+            event?.preventDefault()
+            toast.error('Insira os dados do abrigo')
+        }
+    }
+    return (
+        <>
+            <nav className={styles.sidebar}>
+
+                <NavLink className={({ isActive }) => (isActive ? styles.active : '')}
+                    to="/admin"
+                    end>Meu Abrigo</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? styles.active : '')}
+                    to="/admin/pets"
+                    onClick={validate}>
+                    Pets</NavLink>
+                <NavLink
+                    to="/">Sair</NavLink>
+            </nav>
+            <Toaster position="top-center" richColors={true} />
+        </>
+    )
 }
